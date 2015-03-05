490f5f154373c9f54464d1d41cc59b6b3159fc4e
bnjcbsn <benjaminj67@yahoo.com>
2015-03-04 21:57:33 -0600

    draft update
    success on plot 1

--- my_plotting.R
+++ my_plotting.R
@@ -1,4 +1,19 @@
+##
+## import and modify data
+
 hpc<-read.csv("household_power_consumption.txt", nrows=5, sep=";")
 classes<-sapply(hpc, class)
 hpc<-read.csv("household_power_consumption.txt", sep=";", header = TRUE, colClasses = classes, na.strings="?")
-hist(hpc$Global_active_power, xlab="Global Active Power (kilowatts)", col="red", main="Global Active Power", xlim=6)
+
+names(hpc)[1]<-"dte"
+
+hpc$dte<-as.Date(hpc$dte, "%d/%m/%Y")
+hpc1<-hpc[hpc$dte==as.Date("2007-02-01")|hpc$dte==as.Date("2007-02-02"),]
+
+##plot1 hist
+png(file="mygraphic.png",width=480,height=480)
+hist(hpc1$Global_active_power, xlab="Global Active Power (kilowatts)", col="red", main="Global Active Power")
+dev.off()
+
+## plot 2 line of hourly data
+
