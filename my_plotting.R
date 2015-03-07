054c43bffe839f3dea1207a9e2d748b92279b77f
bnjcbsn <benjaminj67@yahoo.com>
2015-03-07 10:51:26 -0600

    draft update
    edits to plot 4

--- my_plotting.R
+++ my_plotting.R
@@ -42,7 +42,7 @@ dev.off()
 png(file="myplot4.png",width=480,height=480)
 par(mfrow=c(2,2))
 ##4a
-plot(hpc1$dtm, hpc1$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+plot(hpc1$dtm, hpc1$Global_active_power, type="l", ylab="Global Active Power", xlab="")
 ##4b
 plot(hpc1$dtm, hpc1$Voltage, type="l", ylab="Voltage", xlab="datetime")
 ##4c
@@ -52,7 +52,7 @@ plot(hpc1$dtm, hpc1$Voltage, type="l", ylab="Voltage", xlab="datetime")
  lines(hpc1$dtm, hpc1$Sub_metering_1, type="l", col=1)
  lines(hpc1$dtm, hpc1$Sub_metering_2, type="l", col=2)
  lines(hpc1$dtm, hpc1$Sub_metering_3, type="l", col="blue")
- legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), col=c("black", "red", "blue"))}
+ legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), col=c("black", "red", "blue"), bty="n")}
 ##4d
 plot(hpc1$dtm, hpc1$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
 dev.off()
