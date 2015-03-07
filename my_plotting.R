11dfdc2a51fc70f2b54663b8e03c401690b0421f
bnjcbsn <benjaminj67@yahoo.com>
2015-03-07 10:17:19 -0600

    draft update - 4th image
    fourth image draft good, needs edits

--- my_plotting.R
+++ my_plotting.R
@@ -37,4 +37,24 @@ lines(hpc1$dtm, hpc1$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), col=c("black", "red", "blue"))
 dev.off()
 
+
+##plot 4
+png(file="myplot4.png",width=480,height=480)
+par(mfrow=c(2,2))
+##4a
+plot(hpc1$dtm, hpc1$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+##4b
+plot(hpc1$dtm, hpc1$Voltage, type="l", ylab="Voltage", xlab="datetime")
+##4c
+{yrange<-range(hpc1$Sub_metering_1)
+ xrange<-range(hpc1$dtm)
+ plot(xrange, yrange, type="n", ylab="Energy sub metering", xlab="", ylim=yrange, xlim=xrange)
+ lines(hpc1$dtm, hpc1$Sub_metering_1, type="l", col=1)
+ lines(hpc1$dtm, hpc1$Sub_metering_2, type="l", col=2)
+ lines(hpc1$dtm, hpc1$Sub_metering_3, type="l", col="blue")
+ legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), col=c("black", "red", "blue"))}
+##4d
+plot(hpc1$dtm, hpc1$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
+dev.off()
+
 ##strptime(hpc[1,2], "%H:%M:%S")
