11f7c93e63c4684eff68b242b7b306a4c5895a19
bnjcbsn <benjaminj67@yahoo.com>
2015-03-08 10:23:57 -0500

    draft update, clean
    unnecessary lines deleted

--- my_plotting.R
+++ my_plotting.R
@@ -13,10 +13,7 @@ hpc<-read.csv("household_power_consumption.txt", sep=";", header = TRUE, colClas
 
 ## append date and time fields
 hpc$dtm<-as.POSIXct(paste(hpc$Date, hpc$Time), format="%d/%m/%Y %H:%M:%S")
-hpc$dte<-as.POSIXct(hpc$Date, format="%d/%m/%Y")
-hpc$tm<-as.POSIXct(hpc$Time, format="%H:%M:%S")
 hpc$Date0<-as.Date(hpc$Date, "%d/%m/%Y")
-hpc$wd<-(format(hpc$Date0, "%a"))
 
 ## subset for plots
 hpc1<-hpc[hpc$Date0==as.Date("2007-02-01")|hpc$Date0==as.Date("2007-02-02"),]
@@ -43,7 +40,6 @@ lines(hpc1$dtm, hpc1$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), col=c("black", "red", "blue"))
 dev.off()
 
-
 ##plot 4
 png(file="plot4.png",width=480,height=480)
 par(mfrow=c(2,2))
@@ -55,16 +51,10 @@ plot(hpc1$dtm, hpc1$Voltage, type="l", ylab="Voltage", xlab="datetime", lwd=.5)
 {yrange<-range(hpc1$Sub_metering_1)
  xrange<-range(hpc1$dtm)
  plot(xrange, yrange, type="n", ylab="Energy sub metering", xlab="", ylim=yrange, xlim=xrange)
- lines(hpc1$dtm, hpc1$Sub_metering_1, type="l", col=1)
- lines(hpc1$dtm, hpc1$Sub_metering_2, type="l", col=2)
+ lines(hpc1$dtm, hpc1$Sub_metering_1, type="l", col="black")
+ lines(hpc1$dtm, hpc1$Sub_metering_2, type="l", col="red")
  lines(hpc1$dtm, hpc1$Sub_metering_3, type="l", col="blue")
  legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), col=c("black", "red", "blue"), bty="n")}
 ##4d
 plot(hpc1$dtm, hpc1$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
 dev.off()
-
-## test section 
-# png(file="plottest.png",width=480,height=480)
-# par(bty="o")
-# plot(hpc1$dtm, hpc1$Voltage, type="l", ylab="Voltage", xlab="datetime")
-# dev.off()
